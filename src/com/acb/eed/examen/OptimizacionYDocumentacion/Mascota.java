c6251154b320b0fcaeaed23817cf7d6ba8fc7367
Antonio Carmona <antoniocb978@gmail.com>
2022-06-07 17:20:53 +0200

    "refactor: Mascota "

---
+++ src/com/acb/eed/examen/OptimizacionYDocumentacion/Mascota.java
@@ -0,0 +1,15 @@
+package com.acb.eed.examen.OptimizacionYDocumentacion;
+
+public class Mascota {
+
+	protected String name;
+
+	public Mascota() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
