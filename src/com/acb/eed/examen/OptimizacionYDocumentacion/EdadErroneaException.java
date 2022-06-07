eb2e691cba95e28dc518a6a1ea581612eacaa2ff
Antonio Carmona <antoniocb978@gmail.com>
2022-06-07 17:13:14 +0200

    "refactor: nombre del paquete "

---
+++ src/com/acb/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -0,0 +1,13 @@
+package com.acb.eed.examen.OptimizacionYDocumentacion;
+
+public class EdadErroneaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EdadErroneaException(String string) {
+		super(string);
+	}
+}
---
+++ src/com/acb/eed/examen/OptimizacionYDocumentacion/package-info.java
@@ -0,0 +1,1 @@
+package com.acb.eed.examen.OptimizacionYDocumentacion;
